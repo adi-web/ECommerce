1cf2b229ea584c81f58d38b35b3c1f6e612c6410
admir <mucaj00@gmail.com>
2024-06-16 17:04:55 +0200

    added password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth import views
 from .views import SignUpView, MyLoginView
 
 urlpatterns=[
     path("signup/",SignUpView.as_view(),name="signup"),
     path("login/", MyLoginView.as_view(), name="login"),
+    path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
